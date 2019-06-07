a9fa3f2aa90454e17cf5cd6959b11e291b618091
dwloco <davidwis000@gmail.com>
2019-06-07 10:45:31 -0300

    primer commit

---
+++ Assets/Scripts/MiCarta.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiCarta : MonoBehaviour {
+
+    public GameObject prefab = null;
+	// Use this for initialization
+	void Start () {
+        Instantiate(prefab,
+            transform.position,
+            Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
