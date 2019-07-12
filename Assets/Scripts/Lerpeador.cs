f7c5b1c2de3a80d6377cd7ad1c77d50c1f9a891a
dwloco <davidwis000@gmail.com>
2019-07-12 09:36:49 -0300

    movimiento mano bot

---
+++ Assets/Scripts/Lerpeador.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Lerpeador
+{
+    float lerpTime = 1f;
+    float currentLerpTime;
+
+    float moveDistance = 10f;
+
+    Vector3 startPos;
+    Vector3 endPos;
+    GameObject gameObject;
+
+    public void Start(GameObject gameObject, Vector3 posFinal)
+    {
+        this.gameObject = gameObject;
+        startPos = gameObject.transform.position;
+        endPos = posFinal;
+    }
+
+    public void Update()
+    {
+        //reset when we press spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentLerpTime = 0f;
+        }
+
+        //increment timer once per frame
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime > lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        //lerp!
+        float perc = currentLerpTime / lerpTime;
+        gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
+    }
+}
--- Assets/Scripts/ManosController.cs
+++ Assets/Scripts/ManosController.cs
@@ -8,35 +8,43 @@ public class ManosController : MonoBehaviour {
     GameObject manoIzq, manoDer;
     TotemBehaviour totemBehaviour;
     Transform[] posicionInicial = new Transform[2]; //Posicion inicial de las manitos
+    Lerpeador lerp;
     
 
     // Use this for initialization
     void Start()
     {
+        lerp = new Lerpeador();
+        
         manoIzq = transform.GetChild(0).gameObject;
         manoDer = transform.GetChild(1).gameObject;
         totem = TotemManager.instance.totem;
         totemBehaviour = totem.GetComponent<TotemBehaviour>();
         posicionInicial[0] = manoIzq.transform;
         posicionInicial[1] = manoDer.transform;
+
+        lerp.Start(manoDer, totem.transform.position);
     }
 
     // Update is called once per frame
 
     void Update()
     {
+        lerp.Update();
         if (MesaManager.instance.iIndexJugActual == iIndexJug)
         {
             //Debug.Log("Soy un bot consciente! Y mi indice es " + iIndexJug);
             //TODO: Hacer que el bot acerque bien las manos al totem
             /*float fStep = fSpeed * Time.deltaTime;
             if (!totemBehaviour.estaAgarrado()) { //Si nadie lo esta agarrando
+                Debug.Log("Posicion totem: " + totem.transform.position);
+                Debug.Log("Posicion mano: " + manoDer.transform.position);
                 manoDer.transform.position = Vector3.MoveTowards(manoDer.transform.position, totem.transform.position, fStep);
             } else //Si ya lo tengo o si otro jugador lo agarró vuelvo
             {
                 manoDer.transform.position = Vector3.MoveTowards(manoDer.transform.position, posicionInicial[1].position, fStep);
             }*/
+            lerp.Update();
         }
-
     }
 }
