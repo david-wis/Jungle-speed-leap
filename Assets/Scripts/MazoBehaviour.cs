34703587abfb592619d62abc68915529fee9d0a6
abruvila <40462592+ezemati@users.noreply.github.com>
2019-07-19 12:09:17 -0300

    ya puse los mazos normales pero no los acomode xd

--- Assets/Scripts/MazoBehaviour.cs
+++ Assets/Scripts/MazoBehaviour.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 
 public class MazoBehaviour : MonoBehaviour {
     private InteractionBehaviour _intObj;
+
     public int iNroMazo; 
 
     // Use this for initialization
     void Start () {
         _intObj = GetComponent<InteractionBehaviour>();
-    }
 
+    }
 
     void Update() {
         if (_intObj.isPrimaryHovered)
@@ -30,5 +31,27 @@ public class MazoBehaviour : MonoBehaviour {
     public void GenerarCarta()
     {
         EventManager.TriggerEvent("agarrarcarta" + iNroMazo);
+    }
+
+    public void PonerMazos()
+    {
+        switch (iNroMazo)
+        {
+            case 0:
+                _intObj.transform.position = new Vector3();
+                break;
+            case 1:
+                _intObj.transform.position = new Vector3();
+                break;
+            case 2:
+                _intObj.transform.position = new Vector3();
+                break;
+            case 3:
+                _intObj.transform.position = new Vector3();
+                break;
+            default:
+                break;
+        }
+        
     }
 }
