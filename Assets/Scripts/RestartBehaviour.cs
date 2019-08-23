e3e25a89e435e7ae97646d32a01db51ae4fc9a37
Android Studio <ezemati1904@gmail.com>
2019-08-23 11:23:28 -0300

    Boton de restart abajo a la derecha

---
+++ Assets/Scripts/RestartBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Leap.Unity.Interaction;
+
+public class RestartBehaviour : MonoBehaviour {
+
+    private InteractionBehaviour _intObj;
+
+    private void Start()
+    {
+        _intObj = GetComponent<InteractionBehaviour>();
+    }
+
+    private void Update()
+    {
+        if (_intObj.isPrimaryHovered)
+        {
+            RestartGame();
+        }
+    }
+
+
+    /// <summary>
+    /// Reinicia la GameScene
+    /// </summary>
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }    
+}
