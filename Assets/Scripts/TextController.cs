a1ac2f077b2675b5320edf7ea8866e1ef137c956
dwloco <davidwis000@gmail.com>
2019-08-23 11:04:20 -0300

    contador en proceso

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    Text txt;
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
