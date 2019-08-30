7462652ff04b30e67f08aefc68a010583f1755aa
dwloco <davidwis000@gmail.com>
2019-08-30 12:10:18 -0300

    probando raycasts sin exito

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -3,18 +3,23 @@ using Leap.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseController : MonoBehaviour {
     public Sprite manoAbierta, manoCerrada;
+    public Button btnJugar, btnReglas, btnSalir;
     HandModelManager handManager;
     LeapProvider _provider;
     Controller _controlador;
     SpriteRenderer imgMano;
-    
-	// Use this for initialization
-	void Start () {
+    GameObject mano;
+    Camera c;
+
+    // Use this for initialization
+    void Start () {
         handManager = GetComponent<HandModelManager>();
-        imgMano = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        mano = transform.GetChild(0).gameObject;
+        imgMano = mano.GetComponent<SpriteRenderer>();
         _provider = handManager.leapProvider;
         imgMano.sprite = manoAbierta;
 	}
@@ -33,7 +38,34 @@ public class MouseController : MonoBehaviour {
                 imgMano.sprite = manoAbierta;
             }
         }
-	}
+        c = Camera.current;
+        CheckearContactoBoton(btnJugar);
+        CheckearContactoBoton(btnReglas);
+        CheckearContactoBoton(btnSalir);
+    }
+
+    private void CheckearContactoBoton(Button btn)
+    {
+        int x = (int)btn.transform.position.x;
+        int y = (int)btn.transform.position.y;
+        /*if (mano.transform.position.x > x && mano.transform.position.x < (x + btn.GetComponent<RectTransform>().rect.width))
+        {
+            if (mano.transform.position.y > y && mano.transform.position.y < (y + btn.GetComponent<RectTransform>().rect.width)) {
+                Debug.Log("Adentro de " + btn.name);
+            }
+        }*/
+
+        RaycastHit hit;
+        Camera cam = Camera.current;
+        Ray ray = cam.ScreenPointToRay(btn.transform.position);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            Debug.Log("Hay algo en el medio");
+            // Do something with the object that was hit by the raycast.
+        }
+    }
 
     private bool NoExtendido(Finger f)
     {
