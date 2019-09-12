f86f995842ff9b37928470ba4c48f025b6ac49f3
abruvila <abruvila@gmail.com>
2019-09-11 23:35:11 -0300

    mensajito del principio y mensajitos del final achicados

---
+++ Assets/Scripts/MensajeInicial.cs
@@ -0,0 +1,28 @@
+﻿using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MensajeInicial : MonoBehaviour
+{
+
+	public GameObject mensajeInicial;
+	private InteractionBehaviour _intObj;
+
+	// Use this for initialization
+	void Start()
+	{
+		_intObj.GetComponent<InteractionBehaviour>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_intObj.isPrimaryHovered)
+		{
+			Destroy(mensajeInicial);//Eventually, the mensajito stopped thinking
+			Destroy(this);
+		}
+	}
+}
