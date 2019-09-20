0002913f683e526cd159858b5be9b1d5ac1a0664
dwloco <davidwis000@gmail.com>
2019-09-20 10:18:07 -0300

    vr funciona

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public GameObject reglas;
+    public GameObject mano;
 
     public void PlayGame()
     {
+        Destroy(mano);
         int buildIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadSceneAsync(buildIndex + 1, LoadSceneMode.Single);
         //SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(buildIndex+1));
